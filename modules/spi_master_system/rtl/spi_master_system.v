c14d5878f89b431cd293d59fed0c0a16315887ec
vinicarvalhop020 <vinipimpim123@gmail.com>
2026-03-26 19:39:31 -0300

    fix moudle structure

--- modules/spi_master_system/rtl/spi_master_system.v
+++ modules/spi_master_system/rtl/spi_master_system.v
@@ -1,4 +1,4 @@
-module spi_master_system (
+module spi_master_system #(
     parameter N_SLAVES = 4,
     parameter MAX_BITS = 16
 )
@@ -6,21 +6,23 @@ module spi_master_system (
     input clk,
     input reset,
 
-    //comands
+    // Comandos
     input cmd_valid,
-    input [39:0] cmd_data, //[31:30] slave_id, [29] cpol, [28] cpha, [27:20] clk_div, [19:15] n_bits, [14:0] data
+    input [39:0] cmd_data, // Exp: [31:30] slave_id, [29] cpol, [28] cpha, [27:20] clk_div, [19:15] n_bits, [14:0] data
     output cmd_ready,
 
-    //resposta 
-
+    // Resposta 
     output rsp_valid,
-    output [15:0] rsp_data, // data received from slave
+    output [15:0] rsp_data, // Dado recebido do escravo
     input rsp_ready,
 
-    // SPI fisico
+    // SPI Físico
     output sclk,
     output mosi,
     input miso,
     output [N_SLAVES-1:0] cs
 );
 
+    // A lógica interna 
+
+endmodule
